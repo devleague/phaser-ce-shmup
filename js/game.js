67852fe12833f0614f7b31040d83565023a0375f
Jesse Copeland <jesse.copeland@gmail.com>
2018-01-24 08:58:10 -1000

    Load sprite to canvas

--- js/game.js
+++ js/game.js
@@ -2,16 +2,19 @@
   const GAME_WIDTH = 400;
   const GAME_HEIGHT = 400;
   const GAME_CONTAINER_ID = 'game';
+  const GFX = 'gfx';
+
+  let player;
 
   const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
 
   // Core game methods
   function preload() {
-    
+    game.load.spritesheet(GFX, '../assets/shmup-spritesheet-140x56-28x28-tile.png', 28, 28);
   }
 
   function create() {
-    
+    player = game.add.sprite(100, 100, GFX, 8);
   }
 
   function update() {
