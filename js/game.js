577b7c5b57c225d83c2a78e7b257e7694cda9da8
Jesse Copeland <jesse.copeland@gmail.com>
2018-01-24 08:20:36 -1000

    Instantiate game
    Add core methods

--- js/game.js
+++ js/game.js
@@ -1,3 +1,21 @@
 (Phaser => {
-  console.log(Phaser);
+  const GAME_WIDTH = 400;
+  const GAME_HEIGHT = 400;
+  const GAME_CONTAINER_ID = 'game';
+
+  const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
+
+  // Core game methods
+  function preload() {
+    
+  }
+
+  function create() {
+    
+  }
+
+  function update() {
+    
+  }
+
 })(window.Phaser);
